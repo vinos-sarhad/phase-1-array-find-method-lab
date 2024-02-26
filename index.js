ea42f67e1e2635eee0e2ca8f12b6a72411399440
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-26 14:47:19 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+  function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    return winningRecord ? winningRecord.year : undefined; ;
+  }
+console.log(superbowlWin(record))
+
+ 
